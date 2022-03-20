cd8f7ad3a8849e3749238df2700fa323b374ae70
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-03-20 15:27:36 +0700

    Update Tugas-1

--- Tugas-1/Soal-2.py
+++ Tugas-1/Soal-2.py
@@ -1,4 +1,4 @@
 r = int(input("Masukkan jari - jari\t: "))
 phi = 22/7
 lingkaran = phi * r**2
-print("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2.".format(r,lingkaran))
+print("Luas lingkaran dengan jari-jari %d cm adalah %.2f cm\u00b2."%(r,lingkaran))
