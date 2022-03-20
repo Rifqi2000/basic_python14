b4cfe8e78f84639e42c8d3c593c03d8b8fa33540
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-03-20 14:51:28 +0700

    Pertemuan 2

---
+++ Pertemuan 2/file.py
@@ -0,0 +1,43 @@
+# print(5+4)
+# print(10 - 6)
+# print(10%2)
+# print(type(10-5))
+
+# print(float(True))
+x = 'I love AI'
+y = 1945
+z=71.80
+m = True
+n = 2*6/3
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(m))
+print(type(n))
+
+# nama = input("Masukkan nama anda: ")
+# print("Nama Anda adalah: "+ nama)
+
+
+x = "Rifqi mulya kiswanto"
+print(x[1:7:2])
+
+a = "Hello"
+b = "World"
+
+c= a+" "+b
+print(c)
+
+print(9 >= 10)
+print(9!=10)
+
+print("\n\n")
+
+#List
+x = [1,2,3,4,5]
+y = ["Apel","Jeruk","Mangga"]
+z = [1,5.0,"Apel",True]
+print(x)
+print(y)
+print(z)
