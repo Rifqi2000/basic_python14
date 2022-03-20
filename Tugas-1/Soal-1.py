c1f19a5b0a6787b64ae92fad267b8e1d256c9a7e
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-03-20 15:05:25 +0700

    Tugas-1

---
+++ Tugas-1/Soal-1.py
@@ -0,0 +1,5 @@
+nama = input("Masukkan Nama\t: ")
+umur = int(input("Masukkan Umur\t: "))
+tinggi = float(input("Masukkan Tinggi\t: "))
+
+print("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm.".format(nama,umur,tinggi))
---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,4 @@
+r = int(input("Masukkan jari - jari\t: "))
+phi = 22/7
+lingkaran = phi * r**2
+print("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2.".format(r,lingkaran))
