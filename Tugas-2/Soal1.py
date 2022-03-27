88cdfa3562204088052f3c45a4f236a6a492b106
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-03-27 22:48:00 +0700

    Tugas-2

---
+++ Tugas-2/Soal1.py
@@ -0,0 +1,25 @@
+kontak = [["Rifqi","082133172777"]]
+tambah_kontak = []
+while True:
+    print("--Menu--")
+    print("1. Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Keluar")
+
+    pilih = int(input("Pilih menu: "))
+
+    if pilih == 1:
+        for i in range (len(kontak)):
+            print("Nama\t\t: ",kontak[i][0])
+            print("No Telepon\t: ",kontak[i][1])
+    elif pilih ==2:
+        nama = input("Nama\t\t: ")
+        nomor = input("No Telepon\t: ")
+        tambah_kontak.append(nama)
+        tambah_kontak.append(nomor)
+        kontak.append(tambah_kontak)
+    elif pilih == 3:
+        print("Program selesai, sampai jumpa!")
+        break
+    else:
+        print("Menu tidak tersedia")
