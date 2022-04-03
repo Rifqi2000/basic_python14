c78e5fb1e43859af093ea0a7b14300c433cb7bd2
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-04-03 14:12:10 +0700

    update pertemuan 6

--- Pertemuan 6/class.py
+++ Pertemuan 6/class.py
@@ -2,7 +2,11 @@ class Dog:
     breed: None
 
 bulldog = Dog()
+chihuahua = Dog() #cara memanggil nama anjing
+
 
 # print(bulldog.breed)
 bulldog.breed ="Bulldog"
+chihuahua.breed ="Chihuahua"
 print(bulldog.breed)
+print(chihuahua.breed)
