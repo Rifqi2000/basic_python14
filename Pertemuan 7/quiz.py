c8d72c837d4641917c498fa438e605aa7d0edf21
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-04-09 14:41:38 +0700

    QUIZ

---
+++ Pertemuan 7/quiz.py
@@ -0,0 +1,8 @@
+A = [1.5, "Indonesia", 4.0, "AI"]
+B = {1.0, "Hello", (1, 2, 3)}
+C = (3, 4, 5, 1, 5)
+
+
+print(type(A))
+print(type(B))
+print(type(C))
--- Pertemuan 7/scope.py
+++ Pertemuan 7/scope.py
@@ -1,20 +1,32 @@
-# local scope
+# # local scope
+# def myfunc():
+#     x = 100
+#     print(x)
+
+
+# myfunc()
+
+# #global scope
+
+# x = 1000
+
+
+# def myfuncs():
+#     x = 2000
+#     print(x)
+
+
+# myfuncs()
+# print(x)
+
+# x = 300
+
+x=200
 def myfunc():
-    x = 100
+    global x
+    x = 300
     print(x)
 
 
 myfunc()
-
-#global scope
-
-x = 1000
-
-
-def myfuncs():
-    x = 2000
-    print(x)
-
-
-myfuncs()
 print(x)
