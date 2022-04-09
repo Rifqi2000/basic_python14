dc47b48d70a3d955de17ed70b5921b878907fee0
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2022-04-09 14:53:01 +0700

    bilangan

---
+++ Pertemuan 7/bilangan.py
@@ -0,0 +1,9 @@
+bil_ganjil={"1","3","5","7"}
+bil_prima ={"2","3","5","7"}
+
+#union
+print(bil_ganjil.union(bil_prima))
+#intersection
+print(bil_ganjil.intersection(bil_prima))
+#difference
+print(bil_ganjil.difference(bil_prima))
